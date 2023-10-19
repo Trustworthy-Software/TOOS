681b2ae5bae3f7010a3319e42c7653b3fab4e32d
Nadia DAOUDI <nadia.daoudi@uni.lu>
2023-10-19 15:39:17 +0200

    improve past seminars collapsing div

--- js/script.js
+++ js/script.js
@@ -4,5 +4,9 @@ speechHeaders.forEach(header => {
     header.addEventListener('click', () => {
         const details = header.nextElementSibling;
         details.style.display = details.style.display === 'none' || details.style.display === '' ? 'block' : 'none';
+        header.classList.toggle('active');
     });
 });
+
+
+
